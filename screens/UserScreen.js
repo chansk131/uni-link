ef763033b9138bfe7db2786e6743b9f6408035b6
Theerapat Muangpoon <chansk131@gmail.com>
2018-09-05 22:07:48 +0700

    add elevation to followingFollowers

--- screens/UserScreen.js
+++ screens/UserScreen.js
@@ -2,20 +2,21 @@ import React from "react";
 import { Text, View } from "react-native";
 
 import Ionicons from "react-native-vector-icons/Ionicons";
-import { Username } from "../components/Username"
-import { Followers } from "../components/FollowingFollower"
+import { Username } from "../components/Username";
+import { Followers } from "../components/FollowingFollower";
 
 export default class UserScreen extends React.Component {
   render() {
     return (
-      <View style={{
-        flex: 1,
-        backgroundColor: "white",
-        padding: '5%',
-
-      }}>
-        <View style={{flexDirection: 'row'}}>
-          <View style={{width: 100, height: 100}}>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "white",
+          padding: "5%"
+        }}
+      >
+        <View style={{ flexDirection: "row" }}>
+          <View style={{ width: 100, height: 100 }}>
             <Text>UserProfilePic</Text>
           </View>
           <View>
