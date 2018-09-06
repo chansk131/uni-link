2d13ef9301aae38611e269c1bc7537d6f25d3e3c
Theerapat Muangpoon <chansk131@gmail.com>
2018-09-06 22:09:23 +0700

    finished sellingPage but still in sellerPage

--- screens/SellerScreen.js
+++ screens/SellerScreen.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Text, View } from "react-native";
+import {
+  Text,
+  View,
+  TouchableOpacity,
+  StyleSheet,
+  Image,
+  ScrollView
+} from "react-native";
 import { connect } from "react-redux";
 
 import Ionicons from "react-native-vector-icons/Ionicons";
@@ -35,45 +42,110 @@ class SellerScreen extends React.Component {
             <Followers user={this.props.user} />
           </View>
         </View>
-        <View style={{ paddingTop: 10 }}>
-          <View
-            style={{
-              flexDirection: "row",
-              backgroundColor: "white",
-              justifyContent: "center",
-              height: 40,
-              shadowOffset: { width: 1, height: 1 },
-              shadowColor: "grey",
-              shadowOpacity: 0.5,
-              elevation: 1
-            }}
-          >
-            <View
+        <View style={{ paddingTop: 10, flex: 1 }}>
+          <View style={style.soldTab}>
+            <TouchableOpacity
+              onPress={() =>
+                this.setState(() => ({
+                  showUnsold: false
+                }))
+              }
               style={{
                 flex: 1,
                 alignItems: "center",
                 justifyContent: "center"
               }}
             >
-              <Text>Sold</Text>
-            </View>
-            <View
+              {this.state.showUnsold ? (
+                <Text style={{ color: "#9D9B9B" }}>Sold</Text>
+              ) : (
+                <Text>Sold</Text>
+              )}
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() =>
+                this.setState(() => ({
+                  showUnsold: true
+                }))
+              }
               style={{
                 flex: 1,
                 alignItems: "center",
                 justifyContent: "center"
               }}
             >
-              <Text>Unsold</Text>
-            </View>
+              {this.state.showUnsold ? (
+                <Text>Unsold</Text>
+              ) : (
+                <Text style={{ color: "#9D9B9B" }}>Unsold</Text>
+              )}
+            </TouchableOpacity>
           </View>
+          <ScrollView style={{ flex: 1 }}>
+            {this.state.showUnsold ? (
+              <UnsoldItemView showUnsold={this.state.showUnsold} />
+            ) : (
+              <SoldItemView showUnsold={this.state.showUnsold} />
+            )}
+          </ScrollView>
         </View>
       </View>
     );
   }
 }
 
+const SoldItemView = props => (
+  <View style={{ paddingTop: 30, paddingHorizontal: 20 }}>
+    <ListedItem product={"Sold"} />
+    <ListedItem product={"Sold"} />
+    <ListedItem product={"Sold"} />
+    <ListedItem product={"Sold"} />
+  </View>
+);
+
+const UnsoldItemView = props => (
+  <View style={{ paddingTop: 30, paddingHorizontal: 20 }}>
+    <ListedItem product={"Unsold"} />
+    <ListedItem product={"Unsold"} />
+    <ListedItem product={"Unsold"} />
+    <ListedItem product={"Unsold"} />
+  </View>
+);
+
+const ListedItem = props => (
+  <View style={{ flexDirection: "row", alignContent: "flex-start" }}>
+    <Image
+      style={{
+        width: 200,
+        height: 120,
+        borderRadius: 10,
+        marginRight: 10,
+        marginBottom: 30
+      }}
+      source={require("../assets/images/placeholder.png")}
+    />
+    <View>
+      <Text>{props.product}</Text>
+      <Text>$500</Text>
+      <Text>By User</Text>
+    </View>
+  </View>
+);
+
 const mapStateToProps = state => ({
   user: state.user
 });
 export default connect(mapStateToProps)(SellerScreen);
+
+const style = StyleSheet.create({
+  soldTab: {
+    flexDirection: "row",
+    backgroundColor: "white",
+    justifyContent: "center",
+    height: 40,
+    shadowOffset: { width: 1, height: 1 },
+    shadowColor: "grey",
+    shadowOpacity: 0.5,
+    elevation: 1
+  }
+});
