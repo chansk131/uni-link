1db926fb4525dbec261d20e4119be64f20b70ea2
Sophita <chansk131@gmail.com>
2018-10-16 14:24:46 +0100

    Add some of new ui to additemscreen

--- screens/AddItemScreen.js
+++ screens/AddItemScreen.js
@@ -1,5 +1,15 @@
 import React from 'react'
-import { Text, View, TextInput, StyleSheet, Button, Image } from 'react-native'
+import {
+  Text,
+  View,
+  TextInput,
+  StyleSheet,
+  Button,
+  Image,
+  ScrollView,
+  TouchableOpacity,
+} from 'react-native'
+import { CheckBox } from 'react-native-elements'
 import Ionicons from 'react-native-vector-icons/Ionicons'
 import * as firebase from 'firebase'
 import Expo from 'expo'
@@ -114,7 +124,59 @@ class AddItemScreen extends React.Component {
 
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <ScrollView
+        style={{
+          flex: 1,
+          paddingTop: 10,
+          backgroundColor: 'white',
+        }}
+      >
+        <View style={{ marginHorizontal: '5%' }}>
+          <Text style={styles.txtLabel}>Title</Text>
+          <TextInput
+            style={styles.txtInput}
+            onChangeText={value => this.updateInput('name', value)}
+            value={this.state.form.name.value}
+            placeholder="Name of product"
+          />
+          <Text style={styles.txtLabel}>Condition</Text>
+          <TouchableOpacity
+            style={{
+              flexDirection: 'row',
+              alignItems: 'center',
+              marginTop: 10,
+            }}
+            onPress={() => {
+              this.props.navigation.navigate('Condition')
+            }}
+          >
+            <Ionicons name={'md-radio-button-on'} size={20} color={'black'} />
+            <Text> New</Text>
+          </TouchableOpacity>
+          {/* show selected choice and this view can be pressed to go to new selecting radio buttons choices */}
+          <Text style={styles.txtLabel}>Pricing</Text>
+          <TextInput
+            style={styles.txtInput}
+            onChangeText={value => this.updateInput('price', value)}
+            // value={`£ ${this.state.form.price.value}`}
+            placeholder="£9"
+            keyboardType="number-pad"
+          />
+          <Text style={styles.txtLabel}>Prefered selling location</Text>
+          <TextInput
+            style={styles.txtInput}
+            onChangeText={value => this.updateInput('location', value)}
+            // value={this.state.form.location.value}
+            placeholder="Enter location you preferred to sell this product/service"
+          />
+          <Text style={styles.txtLabel}>Product/Service Type</Text>
+          <TextInput
+            style={styles.txtInput}
+            onChangeText={value => this.updateInput('type', value)}
+            // value={this.state.form.type.value}
+            placeholder="Tablet, Novel, Computing, ..."
+          />
+        </View>
         <View style={{ width: '80%', height: '5%' }}>
           <Text>Name</Text>
           <TextInput
@@ -176,7 +238,7 @@ class AddItemScreen extends React.Component {
           />
         )) ||
           null}
-      </View>
+      </ScrollView>
     )
   }
 }
@@ -203,6 +265,21 @@ async function uploadImageAsync(uri, imgId) {
     console.log(e)
   }
 }
+
+const styles = StyleSheet.create({
+  txtLabel: {
+    fontSize: 20,
+    marginTop: 10,
+  },
+  txtInput: {
+    width: '100%',
+    borderBottomWidth: 2,
+    borderBottomColor: '#eaeaea',
+    fontSize: 14,
+    padding: 5,
+    marginTop: 10,
+  },
+})
 
 /*
 https://uni-link-9f8f5.firebaseio.com/products.json/
